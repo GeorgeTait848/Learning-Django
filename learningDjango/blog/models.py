483a809d1ee287180ce790a54e7b6bf0b6eda473
George Tait <george.tait512@gmail.com>
2022-11-20 22:44:47 +0000

    Completed tutorilal 5
    Learned about migrations with the manage.py file and adding to the database via the shell.
    
    How to query the data from db to be used for context when rendering pages. Would be interested to find out how
    to do this with plots. I.e integrate plotly with this.
    
    Also learned about accessing and editing posts by registering them in the admin page.

--- learningDjango/blog/models.py
+++ learningDjango/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model): 
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # now is a function however we dont want to execute it, only pass the function as an argument
+
+    # possible options: 
+    # auto_now = True - upates to current datetime any time a modification is made to the post.
+    #auto_now_add = True - sets to time created and does not defer from this time, however can never update when the class instance was created.
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- learningDjango/blog/views.py
+++ learningDjango/blog/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from . import posts
+from .models import Post
 # Create your views here.
 
 def home(request):
 #this view is called when the user is directed to the blog home page, hence the name home. 
-    context = {
-        'posts': posts.posts,
-        'title': 'Home',
-    }
+    context = {'posts': Post.objects.all(), 'title': 'Home'}
     return render(request, 'blog/home.html', context)
 
 def about(request): 
