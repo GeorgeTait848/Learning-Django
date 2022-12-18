f47a345be8efffcf5d7156b782bfe52c28226432
George Tait <george.tait512@gmail.com>
2022-12-18 21:29:03 +0000

    Completed Tutorial 9
    Added functinality for users to be able to update their profile
    and for the author profile picture to be displayed for a post.

--- learningDjango/users/models.py
+++ learningDjango/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
@@ -9,3 +10,16 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return '{} Profile'.format(self.user.username)
+
+    def save(self):
+        '''overriding the save method to resize image if it is too big'''
+        
+        super().save()
+        img = Image.open(self.image.path)
+
+        #check if this image is more than 300px in height or width
+
+        if img.height > 300 or img.width > 300:
+            outputSz = (300, 300)
+            img.thumbnail(outputSz) # resize image
+            img.save(self.image.path)
